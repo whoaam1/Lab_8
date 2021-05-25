049a9b8b84607d04b0c2e54a752997c61bfe95dd
whoaam1 <yaroslav.shchudlyk.knm.2020@lpnu.ua>
2021-05-25 18:49:47 +0300

    Added files for lab tenth

--- src/main/java/ua/lviv/iot/cars/models/Car.java
+++ src/main/java/ua/lviv/iot/cars/models/Car.java
@@ -1,16 +1,24 @@
 package ua.lviv.iot.cars.models;
 
 import lombok.*;
-
 import ua.lviv.iot.cars.enums.Fuel;
 import ua.lviv.iot.cars.enums.Standard;
 
+import javax.persistence.*;
+
+@Table(name = "cars")
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
-@ToString(callSuper = true)
+@ToString
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
     public Float weight;
     public String color;
     public Integer volume;
--- src/main/java/ua/lviv/iot/cars/models/Sedan.java
+++ src/main/java/ua/lviv/iot/cars/models/Sedan.java
@@ -4,24 +4,39 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import lombok.ToString;
 import ua.lviv.iot.cars.enums.Fuel;
 import ua.lviv.iot.cars.enums.Standard;
 
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
 
 public class Sedan extends Car {
     public String vehicleType;
     public Integer volumeOfTrunk;
 
-    public Sedan(final Float weight, final String color, final Integer volume, final Standard standardType,
-                 final Float engineCapacity, final Integer price, final Fuel fuel, final Integer numberOfWheels,
-                 final String vehicleType, final Integer volumeOfTrunk) {
-        super(weight, color, volume, standardType, engineCapacity, price, fuel, numberOfWheels);
+    public Sedan(Integer id, Float weight, String color, Integer volume, Standard standardType, Float engineCapacity,
+                 Integer price, Fuel fuel, Integer numberOfWheels, String vehicleType, Integer volumeOfTrunk){
+        super(id, weight, color, volume, standardType, engineCapacity, price, fuel, numberOfWheels);
         this.vehicleType = vehicleType;
         this.volumeOfTrunk = volumeOfTrunk;
+    }
+
+    @Override
+    public String toString() {
+        return "Sedan{" +
+                "weight=" + weight +
+                ", color='" + color + '\'' +
+                ", volume=" + volume +
+                ", standardType=" + standardType +
+                ", engineCapacity=" + engineCapacity +
+                ", price=" + price +
+                ", fuel=" + fuel +
+                ", numberOfWheels=" + numberOfWheels +
+                ", standardType=" + standardType +
+                ", fuel=" + fuel +
+                ", vehicleType='" + vehicleType + '\'' +
+                ", volumeOfTrunk=" + volumeOfTrunk +
+                '}';
     }
 }
